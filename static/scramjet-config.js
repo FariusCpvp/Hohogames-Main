e47a54c7d2d59376da4022e1c582ec1f488346fe
FariusCpvp <FariusCpvp@gmail.com>
2026-09-20 20:53:22 +0000

    Remove Scramjet proxy from games site

--- static/scramjet-config.js
+++
@@ -1,8 +0,0 @@
-window.scramjetConfig = {
-    prefix: "/scramjet/",
-    files: {
-        wasm: "/scram/scramjet.wasm.wasm",
-        all: "/scram/scramjet.all.js",
-        sync: "/scram/scramjet.sync.js",
-    },
-};
